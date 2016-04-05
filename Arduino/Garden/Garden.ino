3d74772dab9992bea229fe9daaadb243c4bb24a3
Astill, John <john.astill@sap.com>
2016-04-04 23:50:31 -0400

    Added Moisture code ready for HW

--- Arduino/Garden/Garden.ino
+++ Arduino/Garden/Garden.ino
@@ -1,6 +1,9 @@
 /* 
  *  Garden water controller with Light and Moisture Sensor.
  *  
+ *  The light sensor with 1K resistor
+ *  https://learn.adafruit.com/photocells/using-a-photocell
+ *  
  *  http://www.seeedstudio.com/wiki/Relay_Shield_V2.0#How_Relay_works
  *  
  *  Digital 4 – controls RELAY4’s COM4 pin (located in J4)
@@ -10,7 +13,10 @@
  */
 
 int pumpControl = 4;    // Digital Arduino Pin used to control the motor
-int lightPin = 0;
+int lightPin = 0;       // Analog Pin 0
+int moisturePin = 1;    // Analog Pin 1
+
+#define MINMOISTURE 200
  
 /**
  * Setup the board
@@ -18,6 +24,12 @@ int lightPin = 0;
 void setup()  {
     // declare the pump control pin to be an output so that we can control the relay
     pinMode(pumpControl, OUTPUT);
+
+    // Start with the pump Off
+    turnPumpOff();
+
+    // For debugging
+    Serial.begin(9600);  
 }
  
 /**
@@ -28,10 +40,16 @@ void loop()  {
     // Check the soil moisture every 60s
 
     // Read the light value. The more light there is, the higher this value
-    int lightVal = readLight();
+    int lightValue = readLight();
+    Serial.print("Light reading = ");
+    Serial.println(lightValue);  
 
-    // Control the pump relay
-    if (lightVal > 600) {
+    int moistureValue = readMoisture();
+    Serial.print("Moisture reading = ");
+    Serial.println(moistureValue);  
+
+    // If the ground is too dry, water it
+    if (moistureValue < MINMOISTURE) {
       turnPumpOn();
     } else {
       turnPumpOff();
@@ -45,6 +63,7 @@ void loop()  {
  */
 void turnPumpOn() {
     digitalWrite(pumpControl,HIGH);// NO3 and COM3 Connected (the motor is running)
+    Serial.println("Pump ON");
 }
 
 /**
@@ -52,9 +71,20 @@ void turnPumpOn() {
  */
 void turnPumpOff() {
     digitalWrite(pumpControl,LOW);// NO3 and COM3 Disconnected (the motor is not running)
+    Serial.println("Pump OFF");
 }
 
+/**
+ * Read the value from the analog pin connected to the light sensor
+ */
 int readLight() {
   return analogRead(lightPin);
+}
+
+/**
+ * Read the value from the analog pin connected to the moisture sensor
+ */
+int readMoisture() {
+  return analogRead(moisturePin);
 }
 
